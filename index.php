0afad290cc8f7df5c894fd393a8e68f0e5558fc6
GKolesnikova <dvizenie77@gmail.com>
2022-05-08 22:53:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 require __DIR__ . '\autoload.php';
 
 $ctrl = (empty($_GET['page'])) ? 'Main' : ucfirst($_GET['page']) ;
-$action = (empty($_GET['action'])) ? 'defaultAction' : lcfirst($_GET['action']) . 'Action' ;
+$action = (empty($_GET['action'])) ? 'defaultAction' : lcfirst($_GET['action']) . 'Action';
 
 \App\Loader::start('\App\Controllers\\' . $ctrl, $action);
