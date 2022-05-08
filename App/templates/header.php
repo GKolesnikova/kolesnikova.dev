6d354e32cc6c980811f298406abff3bac47ae894
GKolesnikova <dvizenie77@gmail.com>
2022-05-08 22:55:07 +0200

    Create header.php

---
+++ App/templates/header.php
@@ -0,0 +1,10 @@
+<header>
+	<nav>
+		<ul>
+			<li><a href="/">Home</a></li>
+			<li><a href="/?page=main&action=city">About my city</a></li>
+			<li><a href="/?page=main&action=my">About me</a></li>
+			<li><a href="/?page=main&action=projekte">My projekte</a></li>
+		</ul>
+	</nav>
+</header>
