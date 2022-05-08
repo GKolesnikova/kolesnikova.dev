df427331c8ab052541d069702f2e269b57de1e85
GKolesnikova <dvizenie77@gmail.com>
2022-05-08 22:51:54 +0200

    add new actions

--- App/Controllers/Main.php
+++ App/Controllers/Main.php
@@ -6,6 +6,32 @@ class Main {
 	public function defaultAction () 
 	{
 		$view = new \App\View();
-		$view->render('index.php');
+		echo $view->render('index.php');
 	}
+	
+	public function cityAction () 
+	{
+		$repo = new \App\Models\SightsRepo(
+			new \App\DB()
+		);
+
+		$view = new \App\View();
+		echo $view->render(
+			'about_my_city.php',
+			['sightsList' =>  $repo->getAllSights()]
+		);   
+	}
+	
+	public function myAction () 
+	{
+		$view = new \App\View();
+		echo $view->render('about_me.php');
+	}
+	
+	public function projekteAction () 
+	{
+		$view = new \App\View();
+		echo $view->render('my_projekte.php');
+	}
+	
 }
