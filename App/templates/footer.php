b2d53e2f21c9d638756d79a65a6708defcd9fef4
GKolesnikova <dvizenie77@gmail.com>
2022-05-08 22:54:52 +0200

    Create footer.php

---
+++ App/templates/footer.php
@@ -0,0 +1,11 @@
+<footer>
+	<ul>	
+		<div class="about-city">
+			<li><a class="hamburg" href="https://www.ndr.de/nachrichten/hamburg/index.html">Hamburg news</a></li>
+			<li><a class="hamburg" href="https://www.wetter.com/wetter_aktuell/wettervorhersage/16_tagesvorhersage/deutschland/hamburg/DE0004130.html">Hamburg weather</a></li>	
+		</div>
+		<li>
+			<a class="animal" href="https://www.tag24.de/thema/tiere">News in the animal world</a>
+		</li>	
+	</ul> 		
+</footer>
