7b98a176b4ab709b147540a00c735b2663118e25
GKolesnikova <dvizenie77@gmail.com>
2022-05-08 22:51:32 +0200

    Create Sight.php

---
+++ App/Models/Sight.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Models;
+
+class Sight {
+	public $id;
+	public $title;
+	public $website;
+	public $address;
+	public $hours;
+	public $description;
+	public $google_location_link;		
+}
